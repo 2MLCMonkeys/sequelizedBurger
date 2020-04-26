70f688705bc02ff2052c7abf3a82f2fc38df477c
2mlcmonkeys <2mlcmonkeys@gmail.com>
2020-04-26 12:55:49 -0400

    updating models

--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,7 @@
+// BURGER MODEL
 module.exports = function(sequelize, DataTypes) {
   var Burger = sequelize.define("Burger", {
+    // NAME OF BURGER
     burger_name: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -7,15 +9,16 @@ module.exports = function(sequelize, DataTypes) {
         len: [1]
       }
     },
+    // DEFINES WHETHER OR NOT CUSTOMER HAS EATEN BURGER 
     devoured: {
       type: DataTypes.BOOLEAN,
       allowNull: false,
       defaultValue: false
     }
   });
-
+// LINKS BURGER AND CUSTOMER MODELS
 Burger.associate = function(models) {
-
+    // JOINS BURGER TO CUSTOMER
     Burger.belongsTo(models.Customer, {
       foreignKey: {
       }
--- models/customer.js
+++ models/customer.js
@@ -1,12 +1,14 @@
+// CUSTOMER MODEL
 module.exports = function(sequelize, DataTypes) {
+  // CUSTOMER NAME
     var Customer = sequelize.define("Customer", {
       name: {
         type: DataTypes.STRING
       }
     });
-  
+  // LINKS CUSOMER MODEL TO BURGER MODEL
     Customer.associate = function(models) {
-  
+      // JOINS CUSTOMER TO BURGER
       Customer.hasMany(models.Burger, {
         onDelete: "cascade"
       });
